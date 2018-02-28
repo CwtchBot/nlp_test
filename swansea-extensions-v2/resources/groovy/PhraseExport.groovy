75ac71e7634b05e12c0d6cbfc0c73e06c28df7a6
henry.wang <henry@chi.swan.ac.uk>
2018-02-28 16:20:30 +0000

    Remove app

--- swansea-extensions-v2/resources/groovy/PhraseExport.groovy
+++
@@ -1,8 +0,0 @@
-new File(scriptParams.outputFile).withWriterAppend{ out ->
-for (doc in docs) {
- AnnotationSet anns = doc.getAnnotations("Bio").get("Phrase3")
- for (ann in anns) {
-  println(ann.getFeatures().get("text"))
- }
-}
-}
